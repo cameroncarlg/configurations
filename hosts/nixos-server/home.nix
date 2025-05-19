9ce480d657553c6e76a5fa3406fb18f711f73c8c
cameroncarlg <cameroncarlg@gmail.com>
2025-05-18 21:53:04 -0700

    ll

--- hosts/nixos-server/home.nix
+++ hosts/nixos-server/home.nix
@@ -101,6 +101,7 @@
        hxc = "sudo hx /home/cameron/configurations/hosts/nixos-server/home.nix";
        hxcv = "sudo hx /home/cameron/configurations/hosts/nixos-server/configuration.nix";
        lg = "lazygit";
+       ll = "ls -la";
      };
   };
 

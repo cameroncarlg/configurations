97cb74b2207045a7ffb492d83146390f28a634ce
cameroncarlg <cameroncarlg@gmail.com>
2025-05-20 16:09:39 -0700

    adding helix

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -4,5 +4,6 @@
     ./direnv.nix
     ./ripgrep.nix
     ./lazygit.nix
+    ./git.nix
   ];
 }
---
+++ modules/common/git.nix
@@ -0,0 +1,7 @@
+{
+  programs.git = {
+    enable = true;
+    userName = "cameroncarlg";
+    userEmail = "cameroncarlg@gmail.com";
+  };
+}

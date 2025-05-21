0dd6395a69c7c71f1272a74baf3d07c3ea921aa1
cameroncarlg <cameroncarlg@gmail.com>
2025-05-20 17:13:38 -0700

    fixed emojii

--- modules/linux/starship.nix
+++ modules/linux/starship.nix
@@ -7,7 +7,7 @@
     settings = {
       add_newline = true;
       format = lib.concatStrings [
-        "🖥️ $directory$git_branch> "
+        "💻 $directory$git_branch> "
         "$line_break"
       ];
     };
